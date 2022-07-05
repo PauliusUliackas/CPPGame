0b33ea099ffd33cc1d1b22cf10c82bd66292d71d
Paulius Uliackas <47225635+PauliusUliackas@users.noreply.github.com>
2022-07-05 13:59:35 +0300

    Swat Animations

--- Entities/Swat.cpp
+++ Entities/Swat.cpp
@@ -5,6 +5,15 @@ Swat::Swat(double x, double y) : Enemy(x,y,40,40)
     speed = 5;
     health = 25;
     timer = 30;
+    anime.load("WDL", "Swat",25);
+    anime.scale("WDL", 2,2);
+    anime.select("WDL");
+    anime.flip("WDL", "WDR", true, false);
+
+    anime.load("SDL", "SwatShoot",25);
+    anime.scale("SDL", 2,2);
+    anime.select("SDL");
+    anime.flip("SDL", "SDR", true, false);
 }
 
 Swat::~Swat()
@@ -15,12 +24,16 @@ Swat::~Swat()
 void Swat::render(sf::RenderWindow* g)
 {
     hitbox.render(g, sf::Color::Blue);
+    anime.play(g, hitbox.getX(), hitbox.getY()-40);
 }
 
 void Swat::update()
 {
     Enemy::update();
     timer += DeltaTime::get();
+
+    if(velocity.x > 0) anime.select("WDR");
+    else if(velocity.x < 0) anime.select("WDL");
 }
 
 void Swat::AI(Player* p)
@@ -33,7 +46,7 @@ void Swat::AI(Player* p)
     double y2 = getHB().getY();
 
     double distance= std::sqrt(std::pow(x2-x1,2) + std::pow(y2-y1,2));
-
+    anime.resume();
     if(distance < length)
     {
         if(x1 < x2) velocity.x = speed;
@@ -52,15 +65,18 @@ void Swat::AI(Player* p)
     }
     else
     {
-
         if(timer < 30) return;
         timer = 0;
 
         velocity.x = 0;
         velocity.y = 0;
+
+        if(p->getHB().getX() > hitbox.getX()) anime.select("SDR");
+        else anime.select("SDL");
+
         Bazooka* bz = new Bazooka(0,0,1, false);
         bz->setActive();
-        bz->shoot(sf::Vector2f(hitbox.getX(), hitbox.getY()), sf::Vector2f(p->getHB().getX(), p->getHB().getY()));
+        bz->shoot(sf::Vector2f(hitbox.getX()+hitbox.getWidth(), hitbox.getY()), sf::Vector2f(p->getHB().getX(), p->getHB().getY()));
         skills.push_back(bz);
     }
 }
